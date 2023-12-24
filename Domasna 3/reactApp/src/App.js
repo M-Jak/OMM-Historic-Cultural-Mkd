7abede9afcc7b13925353cf6cd49ec1c07aca159
Nik0la23 <nikolanedeski123@gmail.com>
2023-12-24 15:56:06 +0100

    create about us page

--- Domasna 3/reactApp/src/App.js
+++ Domasna 3/reactApp/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Map from "./Map";
+import AboutUs from "./AboutUs";
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
                 <Route exact path="/" element={<Map/>}/>
                 <Route path='/map' exact={true}
                        element={<Map/>}/>
+                <Route path='/AboutUs' exact={true}
+                       element={<AboutUs/>}/>
             </Routes>
         </Router>
     )
